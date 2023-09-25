eeec3ca17c0fee893995277bd91e5bcd306e974a
sayevvv <shamilbasayev10@gmail.com>
2023-09-25 20:45:54 +0700

    Modifikasi program nilai huruf

--- PemilihanPercobaan201.java
+++ PemilihanPercobaan201.java
@@ -15,9 +15,38 @@ public class PemilihanPercobaan201 {
 
         float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
         
-        String message = total < 65 ? "Remidi" : "Tidak Remidi";
-
-        System.out.println("Nilai akhir = " + total + " sehingga " + message);
+        if (total > 80 && total <= 100) {
+            char nilaiHuruf = 'A';
+            System.out.println("Nilai akhir = " + total + " Nilai Huruf = " + nilaiHuruf);
+        }
+        else if (total <= 80 && total > 73) {
+            String nilaiHuruf = "B+";
+            System.out.println("Nilai akhir = " + total + " Nilai Huruf = " + nilaiHuruf);
+        }
+        else if ( total <= 73 && total > 65) {
+            char nilaiHuruf = 'B';
+            System.out.println("Nilai akhir : " + total + " Nilai Huruf = " + nilaiHuruf);
+        }
+        else if (total <= 65 && total > 60) {
+            String nilaiHuruf = "C+";
+            System.out.println("Nilai akhir = " + total + " Nilai Huruf = " + nilaiHuruf);
+        }
+        else if (total <= 60 && total> 50 ) {
+            char nilaiHuruf = 'C';
+            System.out.println("Nilai akhir = " + total + " Nilai Huruf = " + nilaiHuruf);
+        }
+        else if (total <= 50 && total > 39) {
+            char nilaiHuruf = 'D';
+            System.out.println("Nilai akhir = " + total + " Nilai Huruf = " + nilaiHuruf);
+        }
+        else if (total <= 39 && total >= 0) {
+            char nilaiHuruf = 'E';
+            System.out.println("Nilai akhir + " + total + " Nilai Huruf = " + nilaiHuruf);
+        }
+        else {
+            System.out.println("Error, input nilai anda invalid");
+        }
+        
         input00.close();
     }
 }
